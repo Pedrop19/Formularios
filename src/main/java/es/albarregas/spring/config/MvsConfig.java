08af0b9c71652e52c70e3ea4ebd4d02f3e3975a0
Pedrop19 <prlazaron01@iesalbarregas.es>
2024-02-21 16:53:01 +0100

    Implementacion de seguridad

---
+++ src/main/java/es/albarregas/spring/config/MvsConfig.java
@@ -0,0 +1,14 @@
+package es.albarregas.spring.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvsConfig implements WebMvcConfigurer{
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login");
+    }
+}
